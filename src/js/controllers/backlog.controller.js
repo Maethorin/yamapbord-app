4c0d2207c47014f3f58fdc9dda87d37b75dbc2c8
Márcio Duarte <marcio@inceres.com.br>
2017-10-14 16:56:52 -0300

    Exibindo estória no sprint

--- src/js/controllers/backlog.controller.js
+++ src/js/controllers/backlog.controller.js
@@ -4,10 +4,17 @@ scrumInCeresControllers.controller('BacklogController', ['$rootScope', '$scope',
   $rootScope.selectedProject = null;
   $rootScope.currentController = 'BacklogController';
   $scope.scrollOptions = {scrollX: 'none', scrollY: 'right', preventWheelEvents: true};
+  $scope.completeStoryPopupOpened = false;
+  $scope.selectedStory = null;
 
   Backlog.query(
     function(response) {
       $scope.sprints = response;
     }
   );
+
+  $scope.toggleCompleteStoryPopup = function(story) {
+    $scope.selectedStory = story;
+    $scope.completeStoryPopupOpened = !$scope.completeStoryPopupOpened;
+  };
 }]);
