a21057d2042b4d47deafbcdbc34706f1fac76920
Márcio Duarte <marcio@inceres.com.br>
2019-01-20 19:53:33 -0200

    ProjectStory

---
+++ src/js/resources/project-story.resource.js
@@ -0,0 +1,9 @@
+'use strict';
+
+scrumInCeresResources.factory('ProjectStory', ['$resource', 'appConfig', function ($resource, appConfig) {
+  return $resource(
+    '{0}/users/me/projects/:projectId/stories/:storyId'.format([appConfig.backendURL]),
+    null,
+    {update: {method: 'PUT'}}
+  );
+}]);
